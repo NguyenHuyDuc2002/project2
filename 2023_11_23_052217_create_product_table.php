dc99054191f43638bbfefe04af8c4db41fad6f1f
eesttc <95729167+eesttc@users.noreply.github.com>
2023-11-24 21:30:26 +0700

    Add files via upload

---
+++ 2023_11_23_052217_create_product_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->id('product_id');
+            $table->unsignedBigInteger('cat_id');
+            $table->unsignedBigInteger('brand_id');
+            $table->string('product_name');
+            $table->decimal('original_price',10,2);
+            $table->decimal('selling_price',10,2);
+            $table->string('ram');
+            $table->string('memory');
+            $table->string('system_operating');
+            $table->string('status');
+            $table->string('quantity');
+            $table->string('detail');
+            $table->foreign('cat_id')->references('cat_id')->on('catogory');
+            $table->foreign('brand_id')->references('brand_id')->on('brand');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product');
+    }
+};
